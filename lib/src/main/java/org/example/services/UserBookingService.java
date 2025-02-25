3a194e7ed6d5c346c0ab4fccace2c549abfe81ce
vishvas04 <vishvaskalyan04@gmail.com>
2025-02-25 21:17:58 +0530

    added services functionality

--- lib/src/main/java/org/example/services/UserBookingService.java
+++ lib/src/main/java/org/example/services/UserBookingService.java
@@ -2,11 +2,20 @@ package org.example.services;
 
 import org.example.entities.User;
 
+import java.io.File;
+import java.util.List;
+
 public class UserBookingService
 {
     private User user1;
+    private List<User> userList;
+    private ObjectMapper objectMapper= new ObjectMapper();
+
+    private static final String userPath = "../localDB/users.json";
     public UserBookingService(User user)
     {
         this.user1 = user;
+        File Users= new File(userPath);
+
     }
 }
