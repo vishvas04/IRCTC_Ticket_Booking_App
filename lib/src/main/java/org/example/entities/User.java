409b82b985b60643efe192c50c24bccc59013c11
vishvas04 <vishvaskalyan04@gmail.com>
2025-02-25 18:03:06 +0530

    Addedd new entities that are used

--- lib/src/main/java/org/example/entities/User.java
+++ lib/src/main/java/org/example/entities/User.java
@@ -1,5 +1,9 @@
 package org.example.entities;
 
 public class User {
-    
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> ticketList;
+    private String userID;
 }
