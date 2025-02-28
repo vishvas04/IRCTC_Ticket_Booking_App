3c68ff95369b1cd85f278f51a0218b4398c0ef4b
vishvas04 <vishvaskalyan04@gmail.com>
2025-02-28 22:54:49 +0530

    commit

--- lib/src/main/java/org/example/services/UserBookingService.java
+++ lib/src/main/java/org/example/services/UserBookingService.java
@@ -1,9 +1,13 @@
 package org.example.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.example.entities.User;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class UserBookingService
 {
@@ -12,10 +16,9 @@ public class UserBookingService
     private ObjectMapper objectMapper= new ObjectMapper();
 
     private static final String userPath = "../localDB/users.json";
-    public UserBookingService(User user)
-    {
+    public UserBookingService(User user) throws IOException {
         this.user1 = user;
         File Users= new File(userPath);
-
+        userList=objectMapper.readValue(Users, new TypeReference<List<User>>() {});
     }
 }
